955911be65bcbfcc2ad0ec8a06f11ac22061832f
snoopy <youzak100@gmail.com>
2024-11-20 16:06:10 +0000

    hh

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# define BUFFER_SIZE 42
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
